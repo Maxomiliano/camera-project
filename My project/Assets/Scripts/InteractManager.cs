3921fc63bb1a37a36f37d623ca619e8f063503de
Maxomiliano <max.nebula1@gmail.com>
2025-02-19 13:49:03 -0300

    UI interactable and tmp

--- My project/Assets/Scripts/InteractManager.cs
+++ My project/Assets/Scripts/InteractManager.cs
@@ -1,4 +1,5 @@
 using System;
+using TMPro;
 using UnityEngine;
 
 public class InteractManager : MonoBehaviour
@@ -7,10 +8,26 @@ public class InteractManager : MonoBehaviour
     [SerializeField] private LayerMask interactableLayer;
     [SerializeField] private Camera playerCamera;
 
+    [SerializeField] private CanvasGroup interactCanvas;
+    [SerializeField] private TextMeshProUGUI interactText;
 
     void Start()
     {
         StarterAssets.StarterAssetsInputs.OnInteractTrigger += TryInteract;
+        interactCanvas.alpha = 0;
+    }
+
+    private void Update()
+    {
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out RaycastHit hit, interactDistance, interactableLayer))
+        {
+            if (hit.collider.TryGetComponent(out IInteractable interactable))
+            {
+                ShowInteractUI();
+                return;
+            }
+        }
+        interactCanvas.alpha = 0;
     }
 
     private void TryInteract()
@@ -20,6 +37,20 @@ public class InteractManager : MonoBehaviour
             if (hit.collider.TryGetComponent(out IInteractable interactable))
             {
                 interactable.Interact();
+            }
+        }
+    }
+
+    private void ShowInteractUI()
+    {
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out RaycastHit hit, interactDistance, interactableLayer))
+        {
+            if (hit.collider.TryGetComponent(out IInteractable interactable))
+            {
+                interactCanvas.alpha = 1;
+                //interactText.text = 
+                interactCanvas.transform.position = Camera.main.WorldToScreenPoint(hit.point + Vector3.up * 0.5f);
+                return;
             }
         }
     }
